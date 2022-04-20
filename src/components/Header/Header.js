3d706258a0661b1d231d836fbef272ff3d9bcd90
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-20 18:42:04 +0700

    added style for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,20 @@
+import "./Header.css";
+import logo from "../../images/icons/logo.svg";
+
 export const Header = () => {
-  return <p>Test</p>;
+  return (
+    <div className="header">
+      <a className="header__logo" href="/">
+        <img src={logo} alt="Логотип учебного проекта" />
+      </a>
+      <div className="header__wrap">
+        <a className="header__link" href="/sign-up">
+          Регистрация
+        </a>
+        <a className="header__link header__link_active" href="/sign-in">
+          Войти
+        </a>
+      </div>
+    </div>
+  );
 };

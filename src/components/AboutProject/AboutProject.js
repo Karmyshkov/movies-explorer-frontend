b0078bb3f00c68cbfbc3c325d5eb5dc0f86aa5f6
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-21 17:23:00 +0700

    added style for AboutProject

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,3 +1,31 @@
+import "./AboutProject.css";
+
 export const AboutProject = () => {
-  return <></>;
+  return (
+    <div className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <ul className="about-project__grid">
+        <li className="about-project__column">
+          <h3 className="about-project__headling">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__text">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и
+            финальные доработки.
+          </p>
+        </li>
+        <li className="about-project__column">
+          <h3 className="about-project__headling">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать,
+            чтобы успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <ul className="about-project__progress">
+        <li className="about-project__week about-project__week_backend">1 неделя</li>
+        <li className="about-project__week">4 недели</li>
+        <p className="about-project__hint">Back-end</p>
+        <p className="about-project__hint">Front-end</p>
+      </ul>
+    </div>
+  );
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import { Promo } from "../Promo";
+import { AboutProject } from "../AboutProject";
 
 export const Main = () => {
   return (
     <>
       <Promo />
+      <AboutProject />
     </>
   );
 };

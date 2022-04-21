7ff043fdb6da37d37822fb7dd5f27701b6f5b221
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-21 22:39:07 +0700

    added style for Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,3 +1,36 @@
+import "./Portfolio.css";
+import arrow from "../../images/icons/arrow-link.svg";
+
 export const Portfolio = () => {
-  return <></>;
+  return (
+    <div className="portfolio">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="/">
+            <h4 className="portfolio__text">Статичный сайт</h4>
+          </a>
+          <a className="portfolio__link" href="/">
+            <img className="portfolio__icon" src={arrow} alt="" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="/">
+            <h4 className="portfolio__text">Адаптивный сайт</h4>
+          </a>
+          <a className="portfolio__link" href="/">
+            <img className="portfolio__icon" src={arrow} alt="" />
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a className="portfolio__link" href="/">
+            <h4 className="portfolio__text">Одностраничное приложение</h4>
+          </a>
+          <a className="portfolio__link" href="/">
+            <img className="portfolio__icon" src={arrow} alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };

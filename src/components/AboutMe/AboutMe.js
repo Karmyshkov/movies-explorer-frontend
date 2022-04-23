8b7fa6e893b5c0ce6cdf02e6d005e8edfa4eb9b2
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-24 00:28:58 +0700

    added adaptiv for 768px

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -7,7 +7,7 @@ export const AboutMe = () => {
       <h2 className="about-me__title">Студент</h2>
       <div className="about-me__row">
         <div className="about-me__inner">
-          <p className="about-me__name">Евгений</p>
+          <p className="about-me__name">Виталий</p>
           <p className="about-me__profession">Фронтенд-разработчик, 30 лет</p>
           <p className="about-me__description">
             Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть

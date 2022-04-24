cb7a0bff53564dacf3ae3171f5a7a4eafe0a1e12
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-24 19:24:58 +0700

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import { Header } from "../Header";
 import { Main } from "../Main";
 import { Footer } from "../Footer";
 
 export const App = () => {
-  const [isLogin, setLogin] = useState(true);
   return (
     <div className="wrapper">
       <Header />
-      {isLogin && <Main />}
+      <Main />
       <Footer />
     </div>
   );

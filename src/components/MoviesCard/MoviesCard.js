96a42ea0da5eb4cbaf4852d853f942ad7d364251
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-27 15:59:21 +0700

    refactoring MoviesCard component

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,4 +1,18 @@
 import React from "react";
-export const MoviesCard = () => {
-  return <></>;
+import "./MoviesCard.css";
+
+export const MoviesCard = ({ img, alt, url, title, time }) => {
+  return (
+    <li className="movies-card__item">
+      <a className="movies-card__link" href={url}>
+        <img className="movies-card__img" src={img} alt={alt} />
+      </a>
+      <div className="movies-card__inner">
+        <a className="movies-card__link" href={url}>
+          <h3 className="movies-card__title">{title}</h3>
+        </a>
+        <p className="movies-card__time">{time}</p>
+      </div>
+    </li>
+  );
 };

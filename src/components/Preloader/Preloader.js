b002aa6c59c88a7359d80378ded5960656f6d8f2
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-27 15:40:37 +0700

    added style for Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,3 +1,12 @@
+import React from "react";
+import "./Preloader.css";
+
 export const Preloader = () => {
-  return <></>;
+  return (
+    <div className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </div>
+  );
 };

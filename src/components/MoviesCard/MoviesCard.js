a990e5080f8093dce0078520174c3e0831f1f679
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-05-10 20:22:45 +0700

    added handleSaved function

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MoviesCard.css";
 
 export const MoviesCard = ({ img, alt, title, time }) => {
+  const [isSaved, setSaved] = useState(false);
+  const handleSaved = () => setSaved(!isSaved);
   return (
-    <li className="movies-card__item movies-card__item_saved">
+    <li
+      onClick={handleSaved}
+      className={`movies-card__item ${isSaved && "movies-card__item_saved"}`}
+    >
       <img className="movies-card__img" src={img} alt={alt} />
       <div className="movies-card__inner">
         <h3 className="movies-card__title">{title}</h3>

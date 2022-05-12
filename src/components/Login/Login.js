8851cf142bfc5e1e603e44ead4756a63a15c0de5
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-05-12 22:33:14 +0700

    added adaptiv for signin page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,12 +6,6 @@ export const Login = () => {
   return (
     <form className="login">
       <label className="login__label">
-        <span className="login__heplper-text">Имя</span>
-        <input className="login__input" />
-      </label>
-      <p className="login__error">Что-то пошло не так...</p>
-
-      <label className="login__label">
         <span className="login__heplper-text">E-mail</span>
         <input className="login__input" />
       </label>
@@ -21,7 +15,7 @@ export const Login = () => {
         <span className="login__heplper-text">Пароль</span>
         <input className="login__input" />
       </label>
-      <p className="login__error login__error_show">Что-то пошло не так...</p>
+      <p className="login__error ">Что-то пошло не так...</p>
 
       <button className="login__btn" type="submit">
         Войти

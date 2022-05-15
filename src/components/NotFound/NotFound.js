ae1101db91751a869c6216d395a4faa15f509c2d
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-05-15 16:59:20 +0700

    fix not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./NotFound.css";
 
 export const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <Link to="/" className="not-found__link">
+      <button onClick={() => navigate(-1)} className="not-found__btn">
         Назад
-      </Link>
+      </button>
     </div>
   );
 };

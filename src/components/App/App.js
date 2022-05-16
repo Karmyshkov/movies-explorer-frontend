6ef0212368d526a1d0a9d9ed274431f5795b6b5b
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-05-16 18:43:07 +0700

    added created context in app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
+import { UserContext } from "../../context/UserContext";
 import { Header } from "../Header";
 import { Main } from "../Main";
 import { Movies } from "../Movies";
@@ -29,6 +30,7 @@ export const App = () => {
     location.pathname === "/saved-movies";
 
   const [isLoginIn, setLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
 
   const navigate = useNavigate();
 
@@ -52,7 +54,8 @@ export const App = () => {
   const checkToken = useCallback(() => {
     api
       .getUserData()
-      .then(() => {
+      .then(({ data }) => {
+        setCurrentUser(data);
         setLoggedIn(true);
       })
       .catch((err) => {
@@ -68,27 +71,29 @@ export const App = () => {
 
   return (
     <div className="body">
-      {isRenderHeader && <Header />}
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route exact path="/movies" element={<ProtectedRoute isLoginIn={isLoginIn} />}>
-          <Route path="/movies" element={<Movies />} />
-        </Route>
-        <Route
-          exact
-          path="/saved-movies"
-          element={<ProtectedRoute isLoginIn={isLoginIn} />}
-        >
-          <Route isLoginIn={isLoginIn} path="/saved-movies" element={<SavedMovies />} />
-        </Route>
-        <Route exact path="/profile" element={<ProtectedRoute isLoginIn={isLoginIn} />}>
-          <Route isLoginIn={isLoginIn} path="/profile" element={<Profile />} />
-        </Route>
-        <Route path="/sign-in" element={<Login onLogin={handleLogin} />} />
-        <Route path="/sign-up" element={<Register onregister={handleRegister} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      {isRenderFooter && <Footer />}
+      <UserContext.Provider value={currentUser}>
+        {isRenderHeader && <Header />}
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route exact path="/movies" element={<ProtectedRoute isLoginIn={isLoginIn} />}>
+            <Route path="/movies" element={<Movies />} />
+          </Route>
+          <Route
+            exact
+            path="/saved-movies"
+            element={<ProtectedRoute isLoginIn={isLoginIn} />}
+          >
+            <Route isLoginIn={isLoginIn} path="/saved-movies" element={<SavedMovies />} />
+          </Route>
+          <Route exact path="/profile" element={<ProtectedRoute isLoginIn={isLoginIn} />}>
+            <Route isLoginIn={isLoginIn} path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<Login onLogin={handleLogin} />} />
+          <Route path="/sign-up" element={<Register onregister={handleRegister} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        {isRenderFooter && <Footer />}
+      </UserContext.Provider>
     </div>
   );
 };

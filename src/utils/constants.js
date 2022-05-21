aa3502480d7193d05fd821fb4c1926f3f838a542
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-05-21 21:29:19 +0700

    clear code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
 const BEATFILM_URL = "https://api.nomoreparties.co";
-//const BASE_URL = "https://api.karmyskov-cinema.nomoredomains.work";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.karmyskov-cinema.nomoredomains.work";
 const requiredField = "Обязательное*";
 const incorrectEmail = "Некорректный email";
 const name = "Имя";
